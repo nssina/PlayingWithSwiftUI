6cebdfaf7c31e25348d28c566f039eb47d9756de
nssina <rabie_sina@yahoo.com>
2020-06-26 11:09:52 -0400

    Project 2 of Completed Day 21

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,21 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingEditAlert = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Iraly", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
-        Button(action: {
-            self.showingEditAlert = true
-        }, label: {
-            HStack(spacing: 10) {
-                Image(systemName: "pencil")
-                Text("Edit")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/))
+                            .shadow(color: .black, radius: 2)
+                    })
+                }
+                Spacer()
             }
-        })
-        .alert(isPresented: $showingEditAlert) {
-            Alert(title: Text("Hello SwiftUI"), message: Text("This is some detail message"), dismissButton: .default(Text("Cool!")))
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
         }
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -31,3 +72,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
