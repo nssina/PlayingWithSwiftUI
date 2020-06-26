66fa2b22d4b911d59f645ab757bede038e58ef96
nssina <rabie_sina@yahoo.com>
2020-06-26 08:01:57 -0400

    Completed Day 16 Part 7

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var name = ""
-    @State private var tapCount = 0
+    let students = ["Harry", "Hermione", "Rob"]
+    @State private var selectedStudent = 0
     
     var body: some View {
-        
-        Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
-            Button("Tap Count \(tapCount)") {
-                self.tapCount += 1
+        VStack {
+            Picker("Select your student", selection: $selectedStudent) {
+                ForEach(0 ..< students.count) {
+                    Text(self.students[$0])
+                }
             }
+            Text("You chose: Student # \(students[selectedStudent])")
         }
     }
 }
