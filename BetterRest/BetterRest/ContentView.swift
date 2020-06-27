150e5d3448ecfa8d2b987b5510f14a8d07f5e180
nsdevelopteam <63967267+nsdevelopteam@users.noreply.github.com>
2020-06-27 07:19:51 -0700

    Starting project four part 1 - Day 26

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by nssina on 6/27/20.
+//  Copyright © 2020 Sina Rabiei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
