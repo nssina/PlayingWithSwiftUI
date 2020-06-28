097b36588f2c37cbbf33a5c7b50d0f0df2df726b
nssina <rabie_sina@yahoo.com>
2020-06-28 06:58:48 -0700

    Start day 28

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by nssina on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
