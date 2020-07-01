3b6cff6d5c4e1418555e534356105c93c8e3e480
nssina <rabie_sina@yahoo.com>
2020-07-01 10:47:10 -0700

    Complete day 31

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
                     }
                 }
                 
-                Section(header: Text("How much tip do you want to leave?")) {
+                Section(header: Text("How much tip do you want to leave???")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
