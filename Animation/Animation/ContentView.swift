65f666ab91db9d48ffbc6679d2e843419ad2d433
nssina <rabie_sina@yahoo.com>
2020-07-03 10:24:07 -0400

    Complete day 32 - project 6 - part 2

---
+++ Animation/Animation/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Sina Rabiei on 7/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        Button("Tap Me") {
+            self.animationAmount += 1
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
